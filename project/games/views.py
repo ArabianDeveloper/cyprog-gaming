a36ebefb34f0e07ecac6356f38733db8e2ba3cb6
ArabianDeveloper <arabiandeveloper92@gmail.com>
2023-01-22 21:31:28 +0400

    add template to games and streams

--- project/games/views.py
+++ project/games/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        
+    }
+    return render(request, 'pages/index.html', context)
+
+
+def details(request):
+    context = {
+        
+    }
+    return render(request, 'pages/details.html', context)
+
+
+def browse(request):
+    context = {
+        
+    }
+    return render(request, 'pages/browse.html', context)
--- project/streams/views.py
+++ project/streams/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def streams(request):
+    context = {
+
+    }
+
+    return render(request, 'pages/streams.html', context)
