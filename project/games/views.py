59ddc9e7f64547658370e5f34d5d05adccaa2b09
ArabianDeveloper <arabiandeveloper92@gmail.com>
2023-01-23 11:55:39 +0400

    some changes

--- project/games/views.py
+++ project/games/views.py
@@ -16,6 +16,7 @@ def details(request, id):
     game = get_object_or_404(Game, id = id)
     context = {
         'game' : game,
+        'images' : game.images,
 
     }
     return render(request, 'pages/details.html', context)
